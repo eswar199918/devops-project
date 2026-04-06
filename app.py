3a4b01ff7354114273233516107f082267a922cd
root <root@LAPTOP-3GE8IJ28.localdomain>
2026-04-06 06:32:22 +0000

    edited app.py to perfectly

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "DevOps Project Running 🚀"
+    return "DevOps Project Running perfectly🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
